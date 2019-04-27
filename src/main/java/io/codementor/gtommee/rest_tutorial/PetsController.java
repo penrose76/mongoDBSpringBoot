d4ece1f8ed06d2a8f2698094f5d18d4299d35ed9
Marcin Jaromin <jaromin.marcin@gmail.com>
2019-04-27 15:48:01 +0200

    first commit

---
+++ src/main/java/io/codementor/gtommee/rest_tutorial/PetsController.java
@@ -0,0 +1,50 @@
+package io.codementor.gtommee.rest_tutorial;
+
+import io.codementor.gtommee.rest_tutorial.models.Pets;
+import io.codementor.gtommee.rest_tutorial.repositories.PetsRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pets")
+public class PetsController {
+    @Autowired
+    private PetsRepository repository;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<Pets> getAllPets() {
+        return repository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<Pets> getPetById(@PathVariable("id") ObjectId id) {
+        return Optional.of(repository.findById(id.toHexString()).orElse(new Pets(ObjectId.get(),"Lampart","Kotek","Coś tam")));//.findBy_id(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public void modifyPetById(@PathVariable("id") ObjectId id, @Valid @RequestBody Pets pets) {
+        pets.set_id(id);
+        repository.save(pets);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public Pets createPet(@Valid @RequestBody Pets pets) {
+        pets.set_id(ObjectId.get());
+        repository.save(pets);
+        return pets;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deletePet(@PathVariable ObjectId id) {
+        repository.delete(repository.findBy_id(id));
+    }
+}
---
+++ src/main/java/io/codementor/gtommee/rest_tutorial/repositories/PetsRepository.java
@@ -0,0 +1,16 @@
+package io.codementor.gtommee.rest_tutorial.repositories;
+
+import io.codementor.gtommee.rest_tutorial.models.Pets;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PetsRepository extends MongoRepository<Pets, String> {
+    Pets findBy_id(ObjectId _id);
+    default String terst(){
+        return "";
+    }
+
+    default String terst3(){
+        return "";
+    }
+}
